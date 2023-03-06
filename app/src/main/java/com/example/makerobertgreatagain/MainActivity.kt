1508db2554feb146c4eae3c4d9d80d59d2e492ad
RMeissnerCC <robert.meissner@codecentric.de>
2023-03-06 11:41:12 +0100

    Adding smooth decreasing

--- app/src/main/java/com/example/makerobertgreatagain/MainActivity.kt
+++ app/src/main/java/com/example/makerobertgreatagain/MainActivity.kt
@@ -1,28 +1,58 @@
 package com.example.makerobertgreatagain
 
 import android.os.Bundle
-import android.util.TypedValue
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.Float.max
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainHandler: Handler
+
+    private val minimalFontsize = 10f
+    private var ourFontsize = minimalFontsize
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainHandler = Handler(Looper.getMainLooper())
     }
 
-    fun increaseSizeClick(view: View?) {
+    fun increaseSizeClick(view: View) {
         val text: TextView = findViewById(R.id.textView)
-        text.setTextSize(TypedValue.COMPLEX_UNIT_SP, (text.textSize*1.01).toFloat())
+        ourFontsize += 1F
+        text.textSize = ourFontsize
     }
 
-    fun onSplashPageClick(view: View?) {
+    fun onSplashPageClick(view: View) {
         finish()
     }
 
-    fun resetTextSize(view: View?) {
+    private val updateTextTask = object : Runnable {
+        override fun run() {
+            decreaseTextSize()
+            mainHandler.postDelayed(this, 1000)
+        }
+    }
+
+
+    fun decreaseTextSize() {
         val text: TextView = findViewById(R.id.textView)
-        text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20F)
+        ourFontsize = max(ourFontsize - 1F, minimalFontsize)
+        text.textSize = ourFontsize
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mainHandler.post(updateTextTask)
+    }
+
+
+    fun resetTextSize(view: View) {
+        val text: TextView = findViewById(R.id.textView)
+        text.textSize = minimalFontsize
+        ourFontsize = minimalFontsize
     }
 }
+
